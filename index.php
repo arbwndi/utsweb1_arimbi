869d752e12fa2522287c11f370b486e213a3d252
arbwndi <akunarimbi6272@gmail.com>
2025-11-06 11:09:20 +0700

    proses login dan session

--- index.php
+++ index.php
@@ -23,3 +23,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Polgan Mart</title>
+</head>
+<body>
+    <h2>Polgan Mart</h2>
+    <?php if (!empty($error)) echo "<p style='color:red;'>$error</p>"; ?>
+    <form method="post">
+        Username: <input type="text" name="username" required><br><br>
+        Password: <input type="password" name="password" required><br><br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
